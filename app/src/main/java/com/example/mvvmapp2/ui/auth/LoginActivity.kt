28e11efcfbfc2ee685531e99828e3e642e3b028c
Elijah <elijate@gmail.com>
2020-08-11 09:07:54 +0300

    Add functionality to LoginActivity

--- app/src/main/java/com/example/mvvmapp2/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmapp2/ui/auth/LoginActivity.kt
@@ -3,8 +3,17 @@ package com.example.mvvmapp2.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mvvmapp2.R
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
 class LoginActivity : AppCompatActivity() {
+
+    override val kodein by kodein()
+    private val factory: AuthViewModelFactory by instance()
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
