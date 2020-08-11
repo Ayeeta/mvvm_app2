ac07d05bba83f6410891c9a29eeadbd9dc9b2bb4
Elijah <elijate@gmail.com>
2020-08-11 07:21:32 +0300

    Set up auth activities

--- app/src/main/java/com/example/mvvmapp2/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmapp2/ui/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mvvmapp2
+package com.example.mvvmapp2.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmapp2.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_layout)
     }
 }
---
+++ app/src/main/java/com/example/mvvmapp2/ui/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmapp2.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmapp2.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
